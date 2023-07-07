7266fb437f359b512ac91a69cbd94c7516726289
ArchUsr64 <anshulusr@gmail.com>
2023-07-07 18:16:41 +0530

    implemented game state checking

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,17 @@ let button = document.getElementById("button") as HTMLButtonElement;
 let board_element= document.getElementById("board") as HTMLElement;
 let turn_display= document.getElementById("turn_display") as HTMLElement;
 
-enum SquareValue {
+const enum SquareValue {
   Empty,
   X,
   O
+};
+
+const enum BoardState{
+  O_Wins,
+  X_Wins,
+  Draw,
+  NonTerminate,
 };
 
 const board: SquareValue[] = [
@@ -15,7 +22,55 @@ const board: SquareValue[] = [
   SquareValue.Empty, SquareValue.Empty, SquareValue.Empty,
 ];
 
-function get_board_state(board: SquareValue[]): string{
+function get_board_state(board: SquareValue[]): BoardState{
+  let board_mask = board.map((cell_value) => {
+    if (cell_value == SquareValue.Empty) 
+      return 0 
+    else return 1
+  });
+  let player_1_mask = board.map((cell_value) => {
+    if (cell_value == SquareValue.X) 
+      return 1 
+    else return 0
+  });
+  let player_2_mask = board.map((cell_value) => {
+    if (cell_value == SquareValue.O) 
+      return 1 
+    else return 0
+  });
+  let win_conditions: number[][] = [
+    [1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 1, 0, 1, 0, 1, 0, 0],
+  ];
+  for (const win_condition of win_conditions) {
+    let player_1_flag = true;
+    let player_2_flag = true;
+    for(let i = 0; i < 9; i++) {
+      if (win_condition[i] === 1 && player_1_mask[i] !== 1) {
+        player_1_flag = false;
+      }
+      if (win_condition[i] === 1 && player_2_mask[i] !== 1) {
+        player_2_flag = false;
+      }
+    }
+    if (player_1_flag)
+      return BoardState.X_Wins;
+    if (player_2_flag)
+      return BoardState.O_Wins;
+  }
+  let filled_cell_count = board_mask.filter((value) => value === 1).length;
+  if (filled_cell_count === 9)
+    return BoardState.Draw
+  return BoardState.NonTerminate
+}
+
+function board_to_string(board: SquareValue[]): string{
   let text : string= "";
   for(let i = 0; i < 9; i++) {
     switch(board[i]) {
@@ -64,7 +119,21 @@ button.addEventListener("click", () => {
       turn = true;
       turn_display.textContent = "Player 1's turn";
     }
-    board_element.textContent = get_board_state(board);
+    board_element.textContent = board_to_string(board);
+    let board_state = get_board_state(board);
+    let game_state= () => {
+      switch (board_state) {
+      case BoardState.O_Wins:
+        return "Player 2 Won"
+      case BoardState.X_Wins:
+        return "Player 1 Won"
+      case BoardState.Draw:
+        return "Game Draw"
+      case BoardState.NonTerminate:
+        return "Continue"
+      }
+    };
+    console.log(game_state());
     update_dropdown(board);
     }
 });
